48f3d1cc9f15faa8edec1fd6a247b809dcac894a
ivwelln <151129749+ivwelln@users.noreply.github.com>
2026-03-26 23:29:20 +0300

    Initial Commit

---
+++ Our pulse/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Our pulse
+//
+//  Created by Иван Солончак on 26.03.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Our pulseTests/Our_pulseTests.swift
@@ -0,0 +1,17 @@
+//
+//  Our_pulseTests.swift
+//  Our pulseTests
+//
+//  Created by Иван Солончак on 26.03.2026.
+//
+
+import Testing
+@testable import Our_pulse
+
+struct Our_pulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
